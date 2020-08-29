b9ff1148bd0f2e155ee9c5a75023361e847d6817
laserguy22 <coolmichael26@gmail.com>
2020-08-29 01:41:10 -0400

    Added page icon

--- temperature-web/src/components/SettingsModal.js
+++ temperature-web/src/components/SettingsModal.js
@@ -118,6 +118,7 @@ export default function SettingsModal(props) {
                                     max={1000}
 
                                 />
+                                <div>Icon made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
                             </Grid>
                         </Grid>
                     </Paper>
